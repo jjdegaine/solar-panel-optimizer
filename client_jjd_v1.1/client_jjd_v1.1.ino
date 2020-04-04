a4f3c1116952e84259c8364f23a7aec0d61f6907
jjdegaine <60794697+jjdegaine@users.noreply.github.com>
2020-04-04 09:01:49 +0200

    update

--- client_jjd_v1.1/client_jjd_v1.1.ino
+++ client_jjd_v1.1/client_jjd_v1.1.ino
@@ -629,7 +629,7 @@ void Taskwifi_udp(void *pvParameters)  // This is a task.
         time_udp_now= millis();
         while (long (time_udp_now +5 > millis () ) )
         {
-        do_noting=true    // do nothing
+        do_noting=true;    // do nothing
         }
             
        //delay(5);
