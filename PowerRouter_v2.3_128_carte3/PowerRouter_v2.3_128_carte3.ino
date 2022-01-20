42cff6e5626864960367ad9fad1a39674d541484
jjdegaine <60794697+jjdegaine@users.noreply.github.com>
2022-01-20 16:04:38 +0100

    Update PowerRouter_v2.3_128_carte3.ino

--- PowerRouter_v2.3_128_carte3/PowerRouter_v2.3_128_carte3.ino
+++ PowerRouter_v2.3_128_carte3/PowerRouter_v2.3_128_carte3.ino
@@ -179,7 +179,7 @@ unsigned long timeout_now;
 signed long wait_it_limit = 3 ;  // delay 3msec
 signed long it_elapsed; // counter for delay 3 msec
 
-char periodStep = 71;                            // 68 * 127 = 10msec, calibration using oscilloscope
+char periodStep = 73;                            // 68 * 127 = 10msec, calibration using oscilloscope
 //char periodStep = 68;                            // 68 * 127 = 10msec, calibration using oscilloscope
 
 volatile int i_counter = 0;                              // Variable to use as a counter
