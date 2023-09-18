cf165e2772ed2bb795d2b148befcf0d8ab1652f0
jjdegaine <60794697+jjdegaine@users.noreply.github.com>
2023-09-18 10:00:11 +0200

    Update BT_to_Wifi_mqtt.ino

--- BT_to_Wifi_mqtt/BT_to_Wifi_mqtt.ino
+++ BT_to_Wifi_mqtt/BT_to_Wifi_mqtt.ino
@@ -9,13 +9,15 @@
 BluetoothSerial SerialBT;
 
 
-//String MACadd = "00:19:10:1109:6B:F3";
+
 String MACadd = "00:21:11:01:1D:65";
 uint8_t address[6]  = {0x00, 0x21, 0x11, 0x01, 0x1D, 0x65};
-//uint8_t address[6]  = {0x11, 0x1D, 0xA5, 0x02, 0xC3, 0x22};
 String name = "HC-06";
 char *pin = "1234"; //<- standard pin would be provided by default
 bool connected;
+byte display =0 ; //
+uint16_t position_y =0 ;
+uint16_t xo =0 ;
 
 void setup() {
   Serial.begin(9600);
@@ -53,9 +55,17 @@ void loop() {
 
   if (SerialBT.available()) {
     //Serial.write(SerialBT.read());
-    M5.Lcd.setCursor(0,0);
-    M5.Lcd.clear(BLACK);
+    M5.Lcd.setCursor(0, position_Y);
     M5.Lcd.print(SerialBT.read());
   }
+  position_y=position_y+30 ;
+  display = display + 1 ;
   delay(5);
+
+   if ( display == 4) {
+      M5.Lcd.clear(BLACK);
+      position_y = 0;
+      display = 0 ;
+   }
+    
 }
