89c94029a14b26cef93b1e25587a293fa13ad899
jjdegaine <60794697+jjdegaine@users.noreply.github.com>
2025-07-23 19:16:01 +0200

    Update PowerRouter_v3.0_mqtt.ino

--- PowerRouter_v3.0_mqtt/PowerRouter_v3.0_mqtt.ino
+++ PowerRouter_v3.0_mqtt/PowerRouter_v3.0_mqtt.ino
@@ -495,7 +495,8 @@ display.display();
   Serial.println("Connected to the Wi-Fi network"); 
   // init wifi mqtt
 
-  client.setServer(mqtt_broker, mqtt_port); 
+  //client.setServer(mqtt_broker, mqtt_port); 
+  connect ("routeur", mqtt_broker, mqtt_port, topic, 1, true ) ;
   //client.setCallback(callback); 
   while (!client.connected()) { 
       String client_id = "routeur"; 
