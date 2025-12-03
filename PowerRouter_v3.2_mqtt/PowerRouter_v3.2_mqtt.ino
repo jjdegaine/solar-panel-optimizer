0280c20706cc53dc0b5213abca46d77ba4902b69
jjdegaine <60794697+jjdegaine@users.noreply.github.com>
2025-12-03 14:56:50 +0100

    Update PowerRouter_v3.2_mqtt.ino

--- PowerRouter_v3.2_mqtt/PowerRouter_v3.2_mqtt.ino
+++ PowerRouter_v3.2_mqtt/PowerRouter_v3.2_mqtt.ino
@@ -919,7 +919,7 @@ void Taskwifi_udp(void *pvParameters) // This is a task.
     Serial.println(client.state());
     */
     MQTT_time = millis();
-    
+
     if (Connect_MQTT()) 
     {
         /*
@@ -944,9 +944,9 @@ void Taskwifi_udp(void *pvParameters) // This is a task.
       }
       */
      // for test only
-      if (send_MQTT_10mn == true)
+      if (send_MQTT == true)
       {
-        send_MQTT_10mn = false;
+        send_MQTT = false;
         sprintf(mystring_power_wifi_10mn, "%g", mean_power_MQTT_10mn);
         client.publish(topic_test, mystring_power_wifi_10mn, true);
       }
