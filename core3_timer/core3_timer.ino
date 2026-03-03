f5357313178062a531caf2666f250379c93166d3
jjdegaine <60794697+jjdegaine@users.noreply.github.com>
2026-03-03 13:06:08 +0100

    Update core3_timer.ino

--- core3_timer/core3_timer.ino
+++ core3_timer/core3_timer.ino
@@ -6,6 +6,13 @@
 
 #include <Esp.h>
 
+/OTA
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ElegantOTA.h>
+
+const char* hostname = "ESP32 test timer core 3";
+
 hw_timer_t *timer = NULL;
 volatile bool flag_timer = false;
 
@@ -29,6 +36,32 @@ void setup()
   timerAlarm(timer, 73 , true, 0);
 
   timerStart(timer);
+
+  // init wifi
+  
+  WiFi.setHostname(hostname);  //define hostname
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+    display.drawString(0, 0, "connecting to WiFi...");
+  }
+  Serial.println("Connected to the WiFi network");
+  display.drawString(0, 0, "connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //OTA server
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { 
+    request->send(200, "text/plain", "ESP routeur server OTA esp test timer core 3");
+  });
+
+  server.begin();
+  Serial.println("HTTP server started OTA version 2026_02_28_h13_mn40");
+ 
+  ElegantOTA.begin(&server);  // Start ElegantOTA
+
 }
 
 void loop()
@@ -39,5 +72,10 @@ void loop()
 
     // Code exécuté toutes les 73 us
     //Serial.println("Tick 73us");
+    
+    //OTA
+
+  ElegantOTA.loop();
+  
   }
 }
