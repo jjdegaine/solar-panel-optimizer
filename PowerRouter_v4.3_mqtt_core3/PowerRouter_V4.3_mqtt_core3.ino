e25f60d1a98bd44469365f64923bca67e89de76c
jjdegaine <60794697+jjdegaine@users.noreply.github.com>
2026-03-27 14:53:38 +0100

    Update PowerRouter_V4.3_mqtt_core3.ino

--- PowerRouter_v4.3_mqtt_core3/PowerRouter_V4.3_mqtt_core3.ino
+++ PowerRouter_v4.3_mqtt_core3/PowerRouter_V4.3_mqtt_core3.ino
@@ -831,6 +831,7 @@ void TaskUI(void *pvParameters) // This is the task UI.
         send_MQTT = true; // ready to send 
         xSemaphoreGive(xMutex);
       }
+      vTaskDelay(pdMS_TO_TICKS(10));  // yield : laisse TaskUI écrire send_MQTT
       mean_power_time = millis();
       //Serial.print("mean_power_mq ");
       //Serial.println(mean_power_MQTT); // MQTT data
@@ -854,6 +855,7 @@ void TaskUI(void *pvParameters) // This is the task UI.
         send_MQTT_5mn = true; // ready to send
         xSemaphoreGive(xMutex);
        }
+       vTaskDelay(pdMS_TO_TICKS(10));  // yield : laisse TaskUI écrire send_MQTT
       mean_power_time_5mn = millis();
       Serial.print("mean_power_mq_5mn ");
       Serial.println(mean_power_MQTT_5mn); // MQTT data
@@ -877,6 +879,7 @@ void TaskUI(void *pvParameters) // This is the task UI.
         send_MQTT_10mn = true; // ready to send
         xSemaphoreGive(xMutex);
       }
+      vTaskDelay(pdMS_TO_TICKS(10));  // yield : laisse TaskUI écrire send_MQTT
       mean_power_time_10mn = millis();
       Serial.print("mean_power_mq_10mn ");
       Serial.println(mean_power_MQTT_10mn); // MQTT data
